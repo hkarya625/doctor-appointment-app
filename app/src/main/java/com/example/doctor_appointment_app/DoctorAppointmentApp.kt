70b860f551ee8459ff8eff6577611facfe68d4a9
Himanshu Arya <hkarya625@gmail.com>
2026-01-20 01:08:41 +0530

    final commit

---
+++ app/src/main/java/com/example/doctor_appointment_app/DoctorAppointmentApp.kt
@@ -0,0 +1,8 @@
+package com.example.doctor_appointment_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class DoctorAppointmentApp: Application()
